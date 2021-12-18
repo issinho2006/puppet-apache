2ba023e594057b44ff100b2e2bd892598fe0ae05
root <root@1d3fd72ec51c.mylabserver.com>
2021-12-18 00:32:46 +0000

    Setup params for 2 different OSes

---
+++ manifests/params.pp
@@ -0,0 +1,15 @@
+# @summary A short summary of the purpose of this class
+#
+# Operating-system-related variables and others
+#
+class apache::params {
+  $install_ensure = 'present'
+  case $::osfamily {
+    'RedHat': {
+       $install_name = 'httpd'
+    }
+    'Debian': {
+       $install_name = 'apache2'
+    }
+  }
+}
