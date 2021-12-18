29254396cca12401d981cbf08e2446097bb23972
root <root@1d3fd72ec51c.mylabserver.com>
2021-12-18 19:45:10 +0000

    added config class

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,13 @@
 #   include apache
 class apache (
   String $install_name,
-  String $install_ensure,){
-  include apache::install
+  String $install_ensure,
+  String $config_ensure,
+  String $config_path,)
+  {
+  contain apache::install
+  contain apache::config
+
+  Class['::apache::install']
+  -> Class['::apache::config']
 }
